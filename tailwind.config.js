3309511f97f0ddfcfa1974b64e206f634388a9e3
Brayan-Alexis <brayanzxd@gmail.com>
2023-10-04 02:21:37 -0700

    Initial files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: ['./public/index.html', './src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
