a3c70668039591bfd812f0abf4800090aeaba080
Brayan-Alexis <brayanzxd@gmail.com>
2023-10-07 06:43:42 -0700

    IL: home, recommended, featured stays & faqs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
   darkMode: false, // or 'media' or 'class'
   content: ['./public/index.html', './src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary': '#362FD9',
+        'secondary': '#1AACAC',
+        'tertiary': '#2E97A7',
+      },
+      fontFamily: {
+        'Lato': 'Lato, sans-serif',
+      },
+      backgroundImage: {
+        'bali': "url('/public/img/bali.jpg')",
+        'capeTown': "url('/public/img/cape-town.jpg')",
+        'istanbul': "url('/public/img/istanbul.jpg')",
+        'kyoto': "url('/public/img/kyoto.jpg')",
+        'machuPicchu': "url('/public/img/machu-picchu.jpg')",
+        'mexicoCity': "url('/public/img/mexico-city.jpg')",
+        'newYork': "url('/public/img/new-york.jpg')",
+        'paris': "url('/public/img/paris.jpg')",
+        'prague': "url('/public/img/prague.jpg')",
+        'rioDeJaneiro': "url('/public/img/rio-de-janeiro.jpg')",
+        'rome': "url('/public/img/rome.jpg')",
+        'santorini': "url('/public/img/santorini.jpg')",
+        'sydney': "url('/public/img/sydney.jpg')",
+        'venice': "url('/public/img/venice.jpg')",
+        'sanFrancisco': "url('/public/img/sanFrancisco.jpg')",
+      },
+    },
   },
   variants: {
     extend: {},
